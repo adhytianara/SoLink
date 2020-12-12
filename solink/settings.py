db060595213e18917910828b4f4a5c1d9db80c18
Ronaldi Tjaidianto <ronalditjaidianto01@gmail.com>
2020-12-12 16:40:48 +0800

    add homepage

--- solink/settings.py
+++ solink/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_khi9tlb1y)#5%6$d$t(%rs8913&ojgo%ue=oeg8%dop7gah&a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
