0bf491f09943170735586f9cfad87ca8e87deb5c
Ronaldi Tjaidianto <ronalditjaidianto01@gmail.com>
2020-12-14 23:22:48 +0800

    add input pengiriman mitra

--- shop/mitra_urls.py
+++ shop/mitra_urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .mitra_views import penjualan
+from .mitra_views import penjualan, pengiriman
 
-app_name = 'shop'
+app_name = 'mtr_shop'
 
 urlpatterns = [
-    path('penjualan/', penjualan, name='shoppage'),
+    path('penjualan/', penjualan, name='penjualan'),
+    path('penjualan/input_pengiriman/', pengiriman, name='pengiriman')
 ]
