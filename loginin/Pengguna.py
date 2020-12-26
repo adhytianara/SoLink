6ab9cfd12cc8f76a4672f268f51da373ec76b3bc
aji <ajiinisti@gmail.com>
2020-12-27 06:31:30 +0700

    Update class pengguna

--- loginin/Pengguna.py
+++ loginin/Pengguna.py
@@ -3,8 +3,5 @@ from barang.models import Barang
 
 class Pengguna:
 
-    def __init__(self,namaPemilik):
-        self.namaPemilik = namaPemilik
-    
     def melihatBarang(self):
         return Barang.objects.all()
