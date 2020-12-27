c7235817010e7342b1f0bf464876ee904a768a70
aji <ajiinisti@gmail.com>
2020-12-27 07:43:42 +0700

    [RED] Test Kelola Barang

--- barang/tests.py
+++ barang/tests.py
@@ -49,3 +49,38 @@ class BarangTest(TestCase):
         data = Barang.objects.get(idBarang=100)
         data.updateFoto("ajiinisti2.com/buku.jpg")
         self.assertEqual(data.urlFoto,"ajiinisti2.com/buku.jpg")
+
+class ViewTest(TestCase):
+    
+    def setUp(self):
+        super().setUp()
+
+        user = User.objects.create(username="inisti")
+        user.set_password('mitra123')
+        user.save()
+
+        user.profile.role = "Mitra"
+        user.save()
+        user = authenticate(username="inisti",password='mitra123')
+        
+        Barang.objects.create(namaPemilik="inisti",
+            idBarang=100,namaBarang="buku aji",deskripsiBarang="Ini adalah buku aji",
+            urlFoto="ajiinisti.com/buku.jpg",hargaBarang=50000,jumlahStok=30,rating=0,stokRate=0)
+
+    def testUrlKelolaBarangValid(self):
+        self.client.login(username="inisti",password="mitra123")
+        response = self.client.get('/barang/')
+        self.assertEqual(response.status_code, 200)
+
+    def testKelolaBarangTemplate(self):
+        self.client.login(username="inisti",password="mitra123")
+        response = self.client.get('/barang/')
+        self.assertTemplateUsed(response, 'barang.html')   
+
+    def testKelolaBarangRedirect(self):
+        response = self.client.get('/barang/')
+        self.assertEqual(response.status_code, 302)
+    
+    def testKelolaBarangView(self):
+        response = resolve("/barang/")
+        self.assertEqual(response.func,listBarang)
