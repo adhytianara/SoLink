18c27ccde2395c152bcd7457335f6f2b57595833
aji <ajiinisti@gmail.com>
2020-12-27 07:36:40 +0700

    [RED] Test for Deskripsi

--- shop/tests.py
+++ shop/tests.py
@@ -36,3 +36,25 @@ class ViewTest(TestCase):
         response = self.client.get('/shop/')
         self.assertContains(response, 'buku aji')
         self.assertContains(response, '50000')
+
+    def testUrlDeskripsi(self):
+        self.client.login(username="inisti",password="mitra123")
+        response = self.client.get('/shop/deskripsi/100/')
+        self.assertEqual(response.status_code, 200)
+
+    def testDeskripsiTemplate(self):
+        self.client.login(username="inisti",password="mitra123")
+        response = self.client.get('/shop/deskripsi/100/')
+        self.assertTemplateUsed(response, 'deskripsiBarang.html')   
+
+    def testDeskripsiView(self):
+        response = resolve("/shop/deskripsi/100/")
+        self.assertEqual(response.func,deskripsi)
+
+    def testDeskripsiBarang(self):
+        response = self.client.get('/shop/deskripsi/100/')
+        self.assertContains(response, 'buku aji')
+        self.assertContains(response, '50000')
+        self.assertContains(response, 'inisti')
+        self.assertContains(response, 'Rating')
+        self.assertContains(response, '30')
