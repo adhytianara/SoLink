0b09ca9efe9b5777deb604db879032856dd6d589
Adhytia <adit.14.12.99@gmail.com>
2020-12-28 21:52:57 +0700

    feat: user authentication

--- donasi/views.py
+++ donasi/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from donasi.managerLembagaSosial import LembagaSosialManager
 from donasi.managerDonasi import DonasiManager
@@ -8,74 +9,86 @@ from datetime import date
 
 @csrf_exempt
 def donasipage(request):
-    lsManager = LembagaSosialManager.getInstance()
-    donasiManager = DonasiManager.getInstance()
-    if request.method == "POST":
-        today = date.today()
+    if request.user.is_authenticated:
+        if request.user.profile.role == "Kontributor/Pembeli":
+            lsManager = LembagaSosialManager.getInstance()
+            donasiManager = DonasiManager.getInstance()
+            if request.method == "POST":
+                today = date.today()
 
-        idLs = request.POST['idLs']
-        namaDonatur = request.POST['namaDonatur']
-        jenisBarang = request.POST['jenisBarang']
-        metodePengiriman = request.POST['metodePengiriman']
-        tanggalPengiriman = request.POST['tanggalPengiriman']
-        alamatJemput = request.POST['alamatJemput']
+                idLs = request.POST['idLs']
+                namaDonatur = request.POST['namaDonatur']
+                jenisBarang = request.POST['jenisBarang']
+                metodePengiriman = request.POST['metodePengiriman']
+                tanggalPengiriman = request.POST['tanggalPengiriman']
+                alamatJemput = request.POST['alamatJemput']
 
-        tanggalDonasi = today.strftime("%d/%m/%Y")
-        status = "Menunggu persetujuan lembaga sosial"
-        alasanPembatalan = ""
-        ls = lsManager.getById(idLs)
-      
-        donasiManager.saveNewDonasi(ls,status,alasanPembatalan,jenisBarang,
-        metodePengiriman,tanggalPengiriman,alamatJemput,
-        namaDonatur,tanggalDonasi)
+                tanggalDonasi = today.strftime("%d/%m/%Y")
+                status = "Menunggu persetujuan lembaga sosial"
+                alasanPembatalan = ""
+                ls = lsManager.getById(idLs)
+            
+                donasiManager.saveNewDonasi(ls,status,alasanPembatalan,jenisBarang,
+                metodePengiriman,tanggalPengiriman,alamatJemput,
+                namaDonatur,tanggalDonasi)
 
-    data = lsManager.getAllLembagaSosial()
-    return render(request, 'kontributor-melakukan-donasi/donasipage.html', {'data': data})
+            data = lsManager.getAllLembagaSosial()
+            return render(request, 'kontributor-melakukan-donasi/donasipage.html', {'data': data})
+    return HttpResponse("Kamu harus masuk sebagai <b>Kontributor/Pembeli</b> untuk melihat halaman ini")
 
 
 @csrf_exempt
 def riwayatdonasi(request):
-    donasi = DonasiManager.getInstance()
-    if request.method == "POST":
-        idDonasi = request.POST['idDonasi']
-        alasanPembatalan = request.POST['alasanPembatalan']
+    if request.user.is_authenticated:
+        if request.user.profile.role == "Kontributor/Pembeli":
+            donasi = DonasiManager.getInstance()
+            if request.method == "POST":
+                idDonasi = request.POST['idDonasi']
+                alasanPembatalan = request.POST['alasanPembatalan']
 
-        status = "Donatur mengajukan pembatalan. Sedang diproses admin"
-        donasi.cancelDonasi(idDonasi, status, alasanPembatalan)
+                status = "Donatur mengajukan pembatalan. Sedang diproses admin"
+                donasi.cancelDonasi(idDonasi, status, alasanPembatalan)
 
-    data = donasi.getAllDonasi()
-    return render(request, 'kontributor-melihat-riwayat-donasi/riwayatdonasi.html', {'data': data})
+            data = donasi.getAllDonasi()
+            return render(request, 'kontributor-melihat-riwayat-donasi/riwayatdonasi.html', {'data': data})
+    return HttpResponse("Kamu harus masuk sebagai <b>Kontributor/Pembeli</b> untuk melihat halaman ini")
 
 
 @csrf_exempt
 def formulirdonasi(request):
-    donasi = DonasiManager.getInstance()
-    if request.method == "POST":
-        idDonasi = request.POST['idDonasi']
-        status = request.POST['status']
+    if request.user.is_authenticated:
+        if request.user.profile.role == "Lembaga Sosial":
+            donasi = DonasiManager.getInstance()
+            if request.method == "POST":
+                idDonasi = request.POST['idDonasi']
+                status = request.POST['status']
 
-        if status == "Ditolak":
-            alasanPembatalan = request.POST['alasanPenolakan']
-            donasi.cancelDonasi(idDonasi, status, alasanPembatalan)
-        else:
-            donasi.updateStatusDonasi(idDonasi, status)
+                if status == "Ditolak":
+                    alasanPembatalan = request.POST['alasanPenolakan']
+                    donasi.cancelDonasi(idDonasi, status, alasanPembatalan)
+                else:
+                    donasi.updateStatusDonasi(idDonasi, status)
 
-    data = donasi.getAllDonasi()
-    return render(request, 'lembaga-sosial/ls-formulirdonasi.html', {'data': data})
+            data = donasi.getAllDonasi()
+            return render(request, 'lembaga-sosial/ls-formulirdonasi.html', {'data': data})
+    return HttpResponse("Kamu harus masuk sebagai <b>Lembaga Sosial</b> untuk melihat halaman ini")    
     
 
 @csrf_exempt
 def adminDaftarPembatalandonasi(request):
-    donasi = DonasiManager.getInstance()
-    if request.method == "POST":
-        idDonasi = request.POST['idDonasi']
-        status = request.POST['status']
+    if request.user.is_authenticated:
+        if request.user.profile.role == "Admin":
+            donasi = DonasiManager.getInstance()
+            if request.method == "POST":
+                idDonasi = request.POST['idDonasi']
+                status = request.POST['status']
 
-        donasi.cancelDonasi(idDonasi, status, "")
+                donasi.cancelDonasi(idDonasi, status, "")
 
-    status = "Donatur mengajukan pembatalan. Sedang diproses admin"
-    data = donasi.getDonasibyStatus(status)
-    return render(request, 'admin/admin-daftarPembatalanDonasi.html', {'data': data})
+            status = "Donatur mengajukan pembatalan. Sedang diproses admin"
+            data = donasi.getDonasibyStatus(status)
+            return render(request, 'admin/admin-daftarPembatalanDonasi.html', {'data': data})
+    return HttpResponse("Kamu harus masuk sebagai <b>Admin</b> untuk melihat halaman ini")    
     
 
 def createLembagaSosial(request):
